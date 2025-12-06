d3a1f2660ac84e718286b132f35372f6fddf0666
Bilal Efe Ünlü <bilalefey27@gmail.com>
2025-12-07 00:37:00 +0300

    Hizmetler Sayfasında Grid yapısının temeli oluşturuldu.

--- ispartaapp/lib/constants/services.dart
+++ ispartaapp/lib/constants/services.dart
@@ -10,6 +10,109 @@ class ServicesPage extends StatefulWidget {
 class _ServicesPageState extends State<ServicesPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text("Hizmetlerimiz")),
+        body: Column(
+          children: [
+            GridView.count(
+              crossAxisCount:
+                  2, // Yan yana kaç tane sığsın? (Senin tasarımda 2)
+              crossAxisSpacing: 12, // Yatay boşluk
+              mainAxisSpacing: 12, // Dikey boşluk
+              childAspectRatio:
+                  2.8, // Kartların en/boy oranı (Dikdörtgen olması için)
+              shrinkWrap: true, // İçindeki elemanlar kadar yer kaplasın
+              physics:
+                  const NeverScrollableScrollPhysics(), // Sayfanın kendi kaydırmasını kullansın
+              padding: const EdgeInsets.all(10), // Kenar boşluğu
+              children: [
+                // KARTLAR
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(15),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(15),
+                    onTap: () {
+                      print("1. Karta Tıklandı");
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.directions_bus, size: 30),
+                        SizedBox(width: 10),
+                        Text("1. Kart"),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(15),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(15),
+                    onTap: () {
+                      print("2. Karta Tıklandı");
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.directions_bus, size: 30),
+                        SizedBox(width: 10),
+                        Text("2. Kart"),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(15),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(15),
+                    onTap: () {
+                      print("3. Karta Tıklandı");
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.directions_bus, size: 30),
+                        SizedBox(width: 10),
+                        Text("3. Kart"),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(15),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(15),
+                    onTap: () {
+                      print("4. Karta Tıklandı");
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.directions_bus, size: 30),
+                        SizedBox(width: 10),
+                        Text("4. Kart"),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
